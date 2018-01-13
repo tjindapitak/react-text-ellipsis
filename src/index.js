dcddc8a51c8862e8632f4240c167dd738d4ea368
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-01-13 22:45:10 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -97,7 +97,7 @@ class TextEllipsis extends React.Component {
             this.container = node;
           },
           className: this.props.tagClass,
-          style: Object.assign({ width: '100%', wordWrap: 'break-word' }, this.props.style})
+          style: Object.assign({ width: '100%', wordWrap: 'break-word' }, this.props.style)
         },
         this.props.children,
       )
